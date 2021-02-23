ce7cb8761484580682617a7f060d05bd8078ab3a
Nathanlobo10 <74343482+Nathanlobo10@users.noreply.github.com>
2021-02-23 19:58:17 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//const Engine  = Matter.Engine;
+//const World = Matter.World;
+//const Bodies = Matter.Bodies;
+
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 350, 50, 50);
+
+ground = new ground (360,360,800,20)
+
+
+}
+
+function draw() {
+  background("purple");  
+  drawSprites();
+
+
+
+
+
+
+}
